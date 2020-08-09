088ffad4c554f61581141fde3e133d826ae72df2
sikeda107 <s.ikeda107@gmail.com>
2020-08-09 14:53:59 +0900

    add: Mock初期化処理を追加

--- src/test/java/example/LogOutputTest.java
+++ src/test/java/example/LogOutputTest.java
@@ -9,6 +9,9 @@ import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
 import static org.hamcrest.core.Is.*;
@@ -23,6 +26,7 @@ public class LogOutputTest {
 
     @Before
     public void setUp() {
+        MockitoAnnotations.openMocks(this);
         final Logger logger = Logger.getLogger(LogOutput.class);
         logger.addAppender(mockAppender);
     }
@@ -30,12 +34,11 @@ public class LogOutputTest {
     @Test
     public void outputLogTest() throws Exception {
         LogOutput logOutput = new LogOutput();
+        logOutput.outputLog();
         verify(mockAppender).doAppend(captorLoggingEvent.capture());
         final LoggingEvent loggingEvent = captorLoggingEvent.getValue();
-        
-        assertThat(loggingEvent.getLevel(), is(Level.INFO));
+        assertThat(loggingEvent.getLevel(), is(Level.ERROR));
         //Check the message being logged is correct
-        assertThat(loggingEvent.getFormattedMessage(),
-                is("String a:foo, String b:bar"));
+        assertThat(loggingEvent.getMessage(),is("Hello!!"));
     }
 }
